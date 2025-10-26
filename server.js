5b7bbfa0c3036874dbc6974ac8bf7ab77e2b86b3
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-10-26 21:38:33 +0800

    新增伺服器設定，實作基本的 Express 應用程式，並設置根路由回應 "hello world"

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// 根路由 - 顯示 "hello world"
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// 啟動伺服器
+app.listen(port, () => {
+  console.log(`伺服器正在 http://localhost:${port} 運行`);
+});
